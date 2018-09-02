713885f3d569e56a90895ad0e1ccee247f32d368
Ihor Yemets <i.yemets@p1k.co.uk>
2018-09-02 14:41:43 +0300

    Add missed reexports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { ITranslationsAdapter } from './itranslations-adapter';
 export { TranslationsProvider, TranslationsConsumer } from './translations-context';
 export { Translation, TranslationRenderProp } from './translation';
 export { withTranslations, WithTranslationsProps } from './with-tanslations-hoc';
+export { SlimI18n, TranslationsEvents, Translations } from './slim-i18n';
